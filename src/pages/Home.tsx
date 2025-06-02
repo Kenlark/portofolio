8cc24e3208f0459ef91d0f8323d98537645cde4c
Kenlark <kenzokerachi@hotmail.fr>
2025-06-02 15:50:26 +0200

    feat: Add react-slick carousel to Home component and update styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import Card from "../components/CardProject";
 import "../styles/home.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import logo from "../assets/images/express-logo.png";
 
 function Home() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 5,
+    autoplay: true,
+    cssEase: "linear",
+    autoplaySpeed: 0,
+    arrows: true,
+  };
+
   return (
     <div className="home">
       <div className="home-content">
@@ -9,6 +25,24 @@ function Home() {
         <p>This is the main page of our application.</p>
       </div>
       <Card title="Project" description="description" />
+
+      <Slider {...settings}>
+        <div>
+          <img src={logo} />
+        </div>
+        <div>
+          <h3>Slide 2</h3>
+        </div>
+        <div>
+          <h3>Slide 3</h3>
+        </div>
+        <div>
+          <h3>Slide 4</h3>
+        </div>
+        <div>
+          <h3>Slide 5</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
